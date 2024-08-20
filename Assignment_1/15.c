bfde66e9a117ec53d0316f7fd36d11ece1dc53df
SAILAJ_SHYAM_MCA_SECA_2024-26 <139752636+iamsailajshyam@users.noreply.github.com>
2024-08-20 20:25:21 +0530

    Create 15.c

---
+++ Assignment_1/15.c
@@ -0,0 +1,11 @@
+//Check if a Number is Even or Odd
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+        printf("%
